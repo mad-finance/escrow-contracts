f489309df7c02f1e057b4105dc20451ef02ac2c9
Nate Mueller <inathan.m@gmail.com>
2023-01-20 13:51:46 -0500

    add polygon thing to withdraw fees

--- script/WithdrawFees.s.sol
+++ script/WithdrawFees.s.sol
@@ -11,9 +11,13 @@ contract WithdrawFeesScript is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        address escrowAddress = 0xE99924ee90c832445AdA389554416C2DaCBaEa78; // MUMBAI TESTNET
+        // address escrowAddress = 0xE99924ee90c832445AdA389554416C2DaCBaEa78; // MUMBAI TESTNET
+        // address[] memory tokens = new address[](1);
+        // tokens[0] = 0x11AE455A85DeB9c34E14db1662E269080b408544; // MUMBAI TEST TOKEN
+
+        address escrowAddress = 0x385B33C3127d5AF5F74fB4193a8dFd86D9a4A166; // POLYGON
         address[] memory tokens = new address[](1);
-        tokens[0] = 0x11AE455A85DeB9c34E14db1662E269080b408544; // MUMBAI TEST TOKEN
+        tokens[0] = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174; // USDC
 
         Escrow escrow = Escrow(payable(escrowAddress));
         escrow.withdrawFees(tokens);
