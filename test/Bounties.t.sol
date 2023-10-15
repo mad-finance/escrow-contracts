0e6fd8af5520d974cd5bd0f738c3c9e6ceeb7f20
Nate Mueller <inathan.m@gmail.com>
2023-10-15 02:01:28 -0500

    post to lens (need a v2 profile to run successfully)

--- test/Bounties.t.sol
+++ test/Bounties.t.sol
@@ -16,6 +16,15 @@ import "../src/interfaces/ISuperToken.sol";
 import "openzeppelin/utils/cryptography/ECDSA.sol";
 import "openzeppelin/token/ERC20/ERC20.sol";
 
+interface IHubTest {
+    function nonces(address signer) external returns (uint256);
+    function changeDelegatedExecutorsConfig(
+        uint256 delegatorProfileId,
+        address[] calldata delegatedExecutors,
+        bool[] calldata approvals
+    ) external;
+}
+
 contract BountiesTest is Test {
     using ECDSA for bytes32;
 
@@ -36,8 +45,8 @@ contract BountiesTest is Test {
 
     uint256 public bidderPrivateKey = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80; // hardhat account 1
     uint256 public bidderPrivateKey2 = 0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d; // hardhat account 2
-    address public bidderAddress = vm.addr(bidderPrivateKey);
-    address public bidderAddress2 = vm.addr(bidderPrivateKey2);
+    address public bidderAddress = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    address public bidderAddress2 = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
 
     uint256 bidAmount1 = 75_000;
     uint256 bidAmount2 = 25_000;
@@ -542,5 +551,170 @@ contract BountiesTest is Test {
         );
         assertEq(wmatic.balanceOf(defaultSender), expected3, "Default sender balance is not correct");
         vm.stopPrank();
+    }
+
+    function testSettleRankedBountyPostToLens() public {
+        {
+            vm.prank(bidderAddress);
+            address[] memory executors = new address[](1);
+            executors[0] = defaultSender;
+
+            bool[] memory approvals = new bool[](1);
+            approvals[0] = true;
+
+            IHubTest(lensHub).changeDelegatedExecutorsConfig(40, executors, approvals);
+        }
+
+        vm.startPrank(defaultSender);
+        uint256 bountyAmount = 100_000_000;
+        helperMintApproveTokens(bountyAmount, defaultSender, usdc);
+        uint256 tokenAmountBefore = usdc.balanceOf(defaultSender);
+
+        {
+            uint256 newBountyId = bounties.deposit(address(usdc), bountyAmount);
+
+            address[] memory recipients = new address[](1);
+            recipients[0] = bidderAddress;
+
+            uint256[] memory bids = new uint256[](1);
+            bids[0] = bidAmount1;
+
+            uint256[] memory revShares = new uint256[](1);
+            revShares[0] = 0;
+
+            bytes[] memory paymentSignatures = new bytes[](1);
+            bytes32 digest =
+                keccak256(abi.encode(newBountyId, recipients[0], bids[0], revShares[0])).toEthSignedMessageHash();
+            (uint8 v, bytes32 r, bytes32 s) = vm.sign(bidderPrivateKey, digest);
+            paymentSignatures[0] = abi.encodePacked(r, s, v);
+
+            uint256 nonce = IHubTest(lensHub).nonces(bidderAddress);
+            uint256 deadline = block.timestamp + 100;
+
+            Types.PostParams[] memory posts = new Types.PostParams[](1);
+            posts[0] = Types.PostParams({
+                profileId: 40,
+                contentURI: "ipfs://123",
+                actionModules: new address[](0),
+                actionModulesInitDatas: new bytes[](0),
+                referenceModule: address(0),
+                referenceModuleInitData: bytes("")
+            });
+
+            Types.EIP712Signature[] memory postSignatures = new Types.EIP712Signature[](1);
+            postSignatures[0] = _getSigStruct({
+                signer: bidderAddress,
+                pKey: bidderPrivateKey,
+                digest: _getPostTypedDataHash(posts[0], nonce, deadline),
+                deadline: deadline
+            });
+
+            Bounties.RankedSettleInput memory input = Bounties.RankedSettleInput({
+                bountyId: newBountyId,
+                recipients: recipients,
+                bids: bids,
+                revShares: revShares,
+                paymentSignatures: paymentSignatures,
+                postParams: posts,
+                signatures: postSignatures,
+                fee: 500
+            });
+
+            bounties.rankedSettle(input);
+            bounties.close(newBountyId);
+        }
+
+        assertEq(usdc.balanceOf(bidderAddress), bidAmount1);
+        assertEq(usdc.balanceOf(defaultSender), tokenAmountBefore - bidAmount1);
+        vm.stopPrank();
+    }
+
+    // INTERNAL LENS TYPED DATA AND SIGNATURE HELPERS
+    function _getSigStruct(uint256 pKey, bytes32 digest, uint256 deadline)
+        internal
+        pure
+        returns (Types.EIP712Signature memory)
+    {
+        return _getSigStruct(vm.addr(pKey), pKey, digest, deadline);
+    }
+
+    function _getSigStruct(address signer, uint256 pKey, bytes32 digest, uint256 deadline)
+        internal
+        pure
+        returns (Types.EIP712Signature memory)
+    {
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(pKey, digest);
+        return Types.EIP712Signature(signer, v, r, s, deadline);
+    }
+
+    function _getPostTypedDataHash(
+        uint256 profileId,
+        string memory contentURI,
+        address[] memory actionModules,
+        bytes[] memory actionModulesInitDatas,
+        address referenceModule,
+        bytes memory referenceModuleInitData,
+        uint256 nonce,
+        uint256 deadline
+    ) internal view returns (bytes32) {
+        bytes32 structHash = keccak256(
+            abi.encode(
+                keccak256(
+                    "Post(uint256 profileId,string contentURI,address collectModule,bytes collectModuleInitData,address referenceModule,bytes referenceModuleInitData,uint256 nonce,uint256 deadline)"
+                ),
+                profileId,
+                keccak256(bytes(contentURI)),
+                actionModules,
+                _hashActionModulesInitDatas(actionModulesInitDatas),
+                referenceModule,
+                keccak256(referenceModuleInitData),
+                nonce,
+                deadline
+            )
+        );
+        return _calculateDigest(structHash);
+    }
+
+    function _getPostTypedDataHash(Types.PostParams memory postParams, uint256 nonce, uint256 deadline)
+        internal
+        view
+        returns (bytes32)
+    {
+        return _getPostTypedDataHash({
+            profileId: postParams.profileId,
+            contentURI: postParams.contentURI,
+            actionModules: postParams.actionModules,
+            actionModulesInitDatas: postParams.actionModulesInitDatas,
+            referenceModule: postParams.referenceModule,
+            referenceModuleInitData: postParams.referenceModuleInitData,
+            nonce: nonce,
+            deadline: deadline
+        });
+    }
+
+    function _calculateDigest(bytes32 structHash) internal view returns (bytes32) {
+        bytes32 domainSeparator = keccak256(
+            abi.encode(
+                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+                keccak256("Lens Protocol Profiles"),
+                keccak256(bytes("2")),
+                block.chainid,
+                lensHub
+            )
+        );
+
+        return keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
+    }
+
+    function _hashActionModulesInitDatas(bytes[] memory actionModulesInitDatas) private pure returns (bytes32) {
+        bytes32[] memory actionModulesInitDatasHashes = new bytes32[](actionModulesInitDatas.length);
+        uint256 i;
+        while (i < actionModulesInitDatas.length) {
+            actionModulesInitDatasHashes[i] = keccak256(abi.encode(actionModulesInitDatas[i]));
+            unchecked {
+                ++i;
+            }
+        }
+        return keccak256(abi.encodePacked(actionModulesInitDatasHashes));
     }
 }
