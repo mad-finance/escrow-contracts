a460d6cbdba31b19d58c798497649e2e01c2dfa2
Nate Mueller <inathan.m@gmail.com>
2023-12-19 23:42:03 -0800

    new deploy

--- script/CreateSticker.s.sol
+++ script/CreateSticker.s.sol
@@ -11,16 +11,21 @@ contract CreateSticker is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        RewardNft rewardNft = RewardNft(0xC7924C6Be44c9f663b181989c263b6A15434b3e8);
+        RewardNft rewardNft = RewardNft(0xC85DBF3eEa4b112288dbDCD43C9B8f4DEb1eFb12);
 
         string memory tokenUri = "ipfs://bafkreiduigb4zpsumwhxd3hgslwkr4jgqa2cznzpmycxezyfm4ooasudfq";
-        address creator = 0x63756D38260C6E4f391E75435377493930a8C370;
+        address creator = 0x7F0408bc8Dfe90C09072D8ccF3a1C544737BcDB6;
         address recipient = 0xB00B28559ae01D962dc72B6AaeDA7395cf8A4ecA;
 
         uint256 id = rewardNft.createCollection(tokenUri, creator);
 
         rewardNft.mint(creator, id, 1, "");
         rewardNft.mint(recipient, id, 1, "");
+
+        address[] memory recipients = new address[](2);
+        recipients[0] = 0x7F0408bc8Dfe90C09072D8ccF3a1C544737BcDB6;
+        recipients[1] = 0xB00B28559ae01D962dc72B6AaeDA7395cf8A4ecA;
+        rewardNft.batchMint(recipients, id, "");
 
         vm.stopBroadcast();
     }
